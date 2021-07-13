ad3eba6b84f4bc87391d508b58b0cddb0490b717
OmarDakka <omardakka42@gmail.com>
2021-07-14 02:03:07 +0300

    updated to putting it together

--- Class Component/prop_it_up/src/components/PersonCard.js
+++ Class Component/prop_it_up/src/components/PersonCard.js
@@ -1,13 +1,28 @@
 import React, { Component } from 'react';
 
 class PersonCard extends Component {
-    render () {
-        const {firstName, lastName, age, hairColor} = this.props;
+    constructor(props) {
+        super(props);
+        this.state = {
+            ageClicked: Number(props.age)
+        }
+    }
+
+    ageClick() {
+        this.setState({
+            ageClicked: this.state.ageClicked + 1
+        });
+    }
+
+    render() {
+        const { firstName, lastName, hairColor } = this.props;
+        const { ageClicked } = this.state;
         return (
             <div>
-                <h1>{ lastName }, { firstName }</h1>
-                <p>Age: { age }</p>
-                <p>Hair Color: { hairColor }</p>
+                <h1>{lastName}, {firstName}</h1>
+                <p>Age: {ageClicked}</p>
+                <p>Hair Color: {hairColor}</p>
+                <button onClick={this.ageClick.bind(this)}>Birthday button for {firstName} {lastName}</button>
             </div>
         )
     }
