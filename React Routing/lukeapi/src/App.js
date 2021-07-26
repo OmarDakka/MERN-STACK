730c7186837751d1eb172c2b0219b03e6e50a5f2
OmarDakka <omardakka42@gmail.com>
2021-07-26 14:35:20 +0300

    luke apiwalker

---
+++ React Routing/lukeapi/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Luke from './components/Luke';
+
+function App() {
+  return (
+    <div className="App">
+      <Luke/>
+    </div>
+  );
+}
+
+export default App;
