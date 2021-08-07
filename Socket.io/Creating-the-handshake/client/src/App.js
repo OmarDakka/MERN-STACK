69cfd74dfc6691929c6b6c708107e57df20d16a7
OmarDakka <omardakka42@gmail.com>
2021-08-07 20:05:22 +0300

    chat app assignment

--- Socket.io/Creating-the-handshake/client/src/App.js
+++ Socket.io/Creating-the-handshake/client/src/App.js
@@ -1,20 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
-import io from 'socket.io-client';
 import React, {useState, useEffect} from 'react';
-import Chat from './components/Chat';
+import io from 'socket.io-client';
+import './App.css';
+import Chat from './components/Chat'
+
 function App() {
-  const [socket] = useState(() => io(':8000'));
+
+  const [socket, setState] = useState(io(":8000"));
 
   useEffect(() => {
-    console.log("Is this running?");
+    console.log("Is the App working?")
     socket.on("Welcome", data => console.log(data));
-
-    return () => socket.disconnect(true);
-  }, []);
+  })
   return (
     <div className="App">
-      <h1>Socket Test</h1>
+      <h1>Chat App</h1>
+      <hr/>
       <Chat/>
     </div>
   );
--- Socket.io/Creating-the-handshake/server.js
+++ Socket.io/Creating-the-handshake/server.js
@@ -1,14 +1,32 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
+let allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', "http://localhost:3000");
+    res.header('Access-Control-Allow-Headers', "http://localhost:8000");
+    next();
+  }
+app.use(allowCrossDomain);
 
-const server = app.listen(8000, () =>{
-    console.log("The server is all fired up on port 8000")
-});
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 
-const io = require('socket.io') (server, {cors: true});
+const server = app.listen(8000, () => console.log("Listening to port:8000"));
+
+const io = require('socket.io')(server);
+
+var users = [];
+
 io.on("connection", socket => {
-    console.log("Nice to meet you.(shake hand)");
-    socket.on("event_from_client", data =>{
-        socket.broadcast.emit("send_data_to_all_other_clients", data);
+
+    socket.on("new_user_connecting", data => {
+        users.push(data);
+        socket.broadcast.emit("new_user_announcement",  data + " has joined the chat.");
+    })
+
+    socket.on("new_message_from_client", data => {
+        console.log(data);
+        socket.broadcast.emit("send_message_to_all_other_clients", data);
     });
 });
